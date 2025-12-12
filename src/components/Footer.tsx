f1140b0f0b8460d98f5dfc366e52228904992b7a
Igor Bjelica <igorbjelica@Igors-MacBook-Pro.local>
2025-12-12 17:36:35 +0100

    Deploy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,6 +13,7 @@ const Footer = () => {
           <div className="footer-links">
             <a href="#about">About</a>
             <a href="#experience">Experience</a>
+            {/* <a href="#projects">Projects</a> */}
             <a href="#skills">Skills</a>
             <a href="#contact">Contact</a>
           </div>
